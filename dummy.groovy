ebb43e5fc7dee042e00f79682b5e296d53098bc2
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 03:07:32 +0530

    added azure login

--- dummy.groovy
+++ dummy.groovy
@@ -10,6 +10,27 @@ pipeline {
                 )
             }
         }
+
+        stage('Azure Login') {
+            steps {
+                script {
+                    // Load Azure credentials from Jenkins credentials
+                    withCredentials([azureServicePrincipal(credentialsId: 'e7e08133-d73a-4ace-80b0-d8f7a5251879', variable: 'AZURE_CREDENTIALS')]) {
+                        def credentials = binding['AZURE_CREDENTIALS']
+                        def clientId = credentials.getClientId()
+                        def clientSecret = credentials.getClientSecret()
+                        def subscriptionId = '44072bee-09d8-4fd1-9150-46a2662697d7'
+                        def tenantId = credentials.getTenant()
+
+                        // Login to Azure
+                        sh "az login --service-principal -u ${clientId} -p ${clientSecret} --tenant ${tenantId}"
+                    }
+                }
+            }
+        }
+
+
+        
         stage('init') {
             steps {
                 script {
