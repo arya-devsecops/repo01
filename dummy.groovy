93b2cc7cc3d09bed0ac1decafc3d91b31d5c7cf0
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 01:13:20 +0530

    Create dummy.groovy
    dummy

---
+++ dummy.groovy
@@ -0,0 +1,10 @@
+stage('Checkout') {
+    steps {
+        git(
+            branch: 'main',
+            url: 'https://github.com/Arya5596/repo02',
+            credentialsId: 'github',
+            tool: 'YourGitInstallationName'
+        )
+    }
+}
