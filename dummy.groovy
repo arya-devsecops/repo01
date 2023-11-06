d9e90674b42e721fd6731b9f5f4cdebaece05f23
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 03:11:51 +0530

    Update dummy.groovy

--- dummy.groovy
+++ dummy.groovy
@@ -11,23 +11,17 @@ pipeline {
             }
         }
 
-        stage('Azure Login') {
-            steps {
-                script {
-                    // Load Azure credentials from Jenkins credentials
-                    withCredentials([azureServicePrincipal(credentialsId: 'e7e08133-d73a-4ace-80b0-d8f7a5251879', variable: 'AZURE_CREDENTIALS')]) {
-                        def credentials = binding['AZURE_CREDENTIALS']
-                        def clientId = credentials.getClientId()
-                        def clientSecret = credentials.getClientSecret()
-                        def subscriptionId = '44072bee-09d8-4fd1-9150-46a2662697d7'
-                        def tenantId = credentials.getTenant()
-
-                        // Login to Azure
-                        sh "az login --service-principal -u ${clientId} -p ${clientSecret} --tenant ${tenantId}"
-                    }
-                }
-            }
-        }
+       stage('Azure Login') {
+           steps {
+               script {
+                   // Load Azure credentials from Jenkins credentials
+                   withCredentials([azureServicePrincipal(credentialsId: 'e7e08133-d73a-4ace-80b0-d8f7a5251879', varAzureSubscriptionId: 'AZURE_SUBSCRIPTION_ID', varAzureTenantId: 'AZURE_TENANT_ID', varAzureClientSecret: 'AZURE_CLIENT_SECRET', varAzureClientId: 'AZURE_CLIENT_ID')]) {
+                       // Login to Azure
+                       sh "az login --service-principal -u \$AZURE_CLIENT_ID -p \$AZURE_CLIENT_SECRET --tenant \$AZURE_TENANT_ID"
+                   }
+               }
+           }
+       }
 
 
         
