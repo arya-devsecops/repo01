0f6cb6e9eb461524e516385d00e7c9d293dad874
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 01:22:16 +0530

    removed tools

--- dummy.groovy
+++ dummy.groovy
@@ -7,7 +7,6 @@ pipeline {
                 branch: 'main',
                 url: 'https://github.com/Arya5596/repo02',
                 credentialsId: 'github',
-                tool: 'YourGitInstallationName'
         )
     }
  }
