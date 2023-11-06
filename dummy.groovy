70259d0ef19ca9e85a1107b03ed15115c895e050
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 02:14:10 +0530

    Update dummy.groovy

--- dummy.groovy
+++ dummy.groovy
@@ -1,23 +1,22 @@
 pipeline {
     agent any
     stages {
-    stage('Checkout') {
-        steps {
-            git(
-                branch: 'main',
-                url: 'https://github.com/Arya5596/repo02',
-                credentialsId: 'github',
-        )
-    }
- }
+        stage('Checkout') {
+            steps {
+                git(
+                    branch: 'main',
+                    url: 'https://github.com/Arya5596/repo02',
+                    credentialsId: 'github'
+                )
+            }
+        }
         stage('init') {
             steps {
-                // Use Terraform plugin to initialize Terraform
-#                terraformInit(
-                    //credentialsId: 'e7e08133-d73a-4ace-80b0-d8f7a5251879', // Specify your Terraform credentials
-    sh 'terraform init'
-                )
+                script {
+                    sh 'terraform init'
+                }
             }
         }
     }
 }
+
