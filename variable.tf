c45dc914f2748e88aa2e910f3e1851a3cde53e78
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 22:57:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -62,7 +62,7 @@ variable "sizes" {
 variable "public_key" {
     type = any
     description = ": Path to the public key to be used for ssh access to the VM. Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash.e.g. c : /home/id_rsa.pub."
-    default = "~/.ssh/known_hosts"
+    default = "SHA256:sTOJEnvLf2GIMGjuf0KvXGmeSBpvYukQkRBv+XYfmps "
    
 }
 
