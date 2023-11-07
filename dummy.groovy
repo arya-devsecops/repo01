d75e6e069387154ef2a643dcfa8ac1459773dcd2
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 17:34:26 +0530

    Update dummy.groovy

--- dummy.groovy
+++ dummy.groovy
@@ -16,7 +16,7 @@ pipeline {
                     withCredentials([azureServicePrincipal(credentialsId: 'e7e08133-d73a-4ace-80b0-d8f7a5251879', 
                                     varAzureSubscriptionId: '44072bee-09d8-4fd1-9150-46a2662697d7',
                                     varAzureTenantId: '3cc22669-453b-4e89-8eb5-29528e2c8a69',
-                                    varAzureClientSecret: 'bby8Q~PkAce0duYn8MmipT7~9UT0uUSpiKzKIbSP',
+                                    varAzureClientSecret: 'E0b8Q~y1dwfQcsFVOTngeOpsSrvbUgwSA.XVKcV5',
                                     varAzureClientId: '99442c11-70a0-4ce9-a51d-1dbdc4f7d339')]) {
                         sh "az login --service-principal -u \$AZURE_CLIENT_ID -p \$AZURE_CLIENT_SECRET --tenant \$AZURE_TENANT_ID"
                     }
