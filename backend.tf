6c2dc5fa36df209364171f7b8233effd12b7ba02
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 23:42:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "group_az"
+    resource_group_name   = "az_exegroup"
     storage_account_name  = "dummyterraform"
     container_name        = "terrraformstate"
     key                   = "sample/vm"
