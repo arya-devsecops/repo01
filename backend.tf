00bfdb22e39ba74fdac5f84561d6c67a901290f3
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 23:54:38 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "az_exegroup"
-    storage_account_name  = "dummyterraform"
-    container_name        = "terrraformstate"
-    key                   = "sample/vm"
-  }
-}
