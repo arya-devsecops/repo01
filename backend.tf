23a1bb7e8a3d5461ada5ce0e5fc1b3e60f1312d4
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-08 18:45:16 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "az_exegroup"  
-    storage_account_name = "myterraformstorageaccount"
-    container_name       = "az_storage"
-    key                  = "terraform.tfstate"
-  }
-}
